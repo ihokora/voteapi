c9ba9b91111b7d1d0ab873a2e66ba5d2a056f823
sikigo <sikoraigo@gmail.com>
2015-08-07 16:22:09 +0300

    add like increment and title route

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'bundler'
 Bundler.require
+set :default_charset, 'utf-8'
 
-
- configure :development do
+configure :development do
     DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.sqlite")
 end
 
@@ -19,17 +19,18 @@ end
 class Song
   include DataMapper::Resource
 
-  property :id, Serial, :key => true
+  property :id, Serial
+  property :title, String
+  property :like, Integer, :default => 0
   property :created_at, DateTime
-  property :title, String, :length => 255
   #property :description, Text
 end
 
 # Finalize the DataMapper models.
 DataMapper.finalize
-
 # Tell DataMapper to update the database according to the definitions above.
 DataMapper.auto_upgrade!
+
 
 get '/' do
   send_file './public/index.html'
@@ -38,30 +39,26 @@ end
 # Route to show all Songs, ordered like a blog
 get '/songs' do
   content_type :json
-  @songs = Song.all(:order => :created_at.desc)
-
+  @songs = Song.all(:order => :likes.desc)
   @songs.to_json
 end
 
 # CREATE: Route to create a new Song
 post '/songs' do
-  content_type :json
-
-  # These next commented lines are for if you are using Backbone.js
-  # JSON is sent in the body of the http request. We need to parse the body
-  # from a string into JSON
+  content_type :json  
   #params_json = JSON.parse(request.body.read)
+ 
+  @song = Song.first_or_create(params)
 
-  # If you are using jQuery's ajax functions, the data goes through in the
-  # params.
-  @song = Song.new(params)
+  @song.like = @song.like.next #Incrementing the number of likes by one.
 
   if @song.save
     @song.to_json
   else
-    halt 500
+    halt 500, { :error => "500" }.to_json
   end
 end
+
 
 # READ: Route to show a specific Song based on its `id`
 get '/songs/:id' do
@@ -76,38 +73,66 @@ get '/songs/:id' do
   end
 end
 
-get '/songs/:title' do
+
+
+
+get '/title/:title' do
   content_type :json
-  @song = Song.get(params[:title].to_s)
+    
+  @song = Song.first(:title => params[:title].to_s)
 
   if @song
     @song.to_json
   else
-    halt 404
+    halt 404, { :error => "do not work with title(" }.to_json
   end
 end
+
+
+
+
+
+
+
+
+# Route to show 5 Top Songs based on amount of their likes
+get '/getTop' do
+  content_type :json
+
+  @topsongs = Song.all(:limit => 5, :order => [:likes.desc])
+
+  if @topsongs
+    @topsongs.to_json
+  else
+    halt 404, {:error => "no top 404"}.to_json
+  end
+end
+
+
+
+
+
 
 # UPDATE: Route to update a Song
 put '/songs/:id' do
   content_type :json
 
-  # These next commented lines are for if you are using Backbone.js
-  # JSON is sent in the body of the http request. We need to parse the body
-  # from a string into JSON
-  #params_json = JSON.parse(request.body.read)
-
-  # If you are using jQuery's ajax functions, the data goes through in the
-  # params.
-
   @song = Song.get(params[:id].to_i)
-  @song.update(params)
+  @song.title = (params[:title])
+  @song.likes = (params[:likes])
+  @song.save
 
   if @song.save
     @song.to_json
   else
-    halt 500
+    halt 500, { :error => "500" }.to_json
   end
 end
+
+
+
+
+
 
 # DELETE: Route to delete a Song
 delete '/songs/:id/delete' do
@@ -117,7 +142,7 @@ delete '/songs/:id/delete' do
   if @song.destroy
     {:success => "ok"}.to_json
   else
-    halt 500
+    halt 500, { :error => "500" }.to_json
   end
 end
 
